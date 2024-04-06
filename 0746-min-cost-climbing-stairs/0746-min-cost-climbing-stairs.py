2a177ad6380ac6cadfb6bbf2863f25305dc1e47f
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-04-06 14:31:39 +0900

    Time: 40 ms (24.97%), Space: 14.3 MB (5.63%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
-        n = len(cost)
         memo = {}
+        n = len(cost)
 
         def dp(n):
             if n == 0 or n == 1:
                 return 0
             if n not in memo:
-                memo[n] = min(dp(n-1) + cost[n-1], dp(n-2) + cost[n-2])
+                memo[n] = min(cost[n-1] + dp(n-1), cost[n-2] + dp(n-2))
             return memo[n]
         return dp(n)
         
