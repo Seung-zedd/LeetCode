22e7a0d4d006b95ffe1048daa9ea32b1b710c725
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-06-27 12:53:12 +0900

    Time: 489 ms (59.7%), Space: 11.8 MB (98.66%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,50 +1,39 @@
 class Solution(object):
     def shortestPathBinaryMatrix(self, grid):
-        r = len(grid) # row
-        c = len(grid[0]) # col
-        visited = [[False]*c for i in range(r)]
+        m = len(grid) # row
+        n = len(grid[0]) # col
+        visited = [[False]*n for _ in range(m)]
         shortest_path = -1
 
-        # (0,0)이 1이거나 (r-1,c-1)이 1이면 막혀서 갈 수 없음
-        if grid[0][0] == 1 or grid[r-1][c-1] == 1:
+        # 시점 또는 종점이 1이면 -1을 리턴
+        if grid[0][0] == 1 or grid[m-1][n-1] == 1:
             return shortest_path
-        
-        def bfs(start_x, start_y):
-            # 사전 세팅
-            visited[start_x][start_y] = True
-            q = deque()
-            q.append((start_x, start_y, 1))
 
-            # row, col의 인덱스에 맞춤(아래, 오른쪽으로 갈수록 증가)
-            # 11시 방향부터 시작
-            dr = [-1, -1, -1, 0, 1, 1, 1, 0]
-            dc = [-1, 0, 1, 1, 1, 0, -1, -1]
-
-            while q:
-                # len 추가 이유: bfs(인접 노드 탐색)할 때 최단거리(대각선으로 가는 경우) 발견하기 위함
-                cur_x, cur_y, cur_len = q.popleft() # (0,0,1)
-                shortest_path = cur_len
-
-                # 종료 조건: 
-                if (cur_x, cur_y) == (r-1, c-1): 
-                    return shortest_path
-
-                for i in range(8):
-                    next_x = cur_x + dr[i]
-                    next_y = cur_y + dc[i]
-                
-                # set a boundary
-                    if 0 <= next_x <= r-1 and 0 <= next_y <= c-1:
-                        # grid의 값이 0이면 방문
-                        if grid[next_x][next_y] == 0 and visited[next_x][next_y] == False:
-                            q.append((next_x, next_y, cur_len + 1))
-                            visited[next_x][next_y] = True
-            # 인접 노드를 다 탐색했는데 도착점에 도달하지 못했으면 -1을 리턴
-            return -1
-
-        # grid 순회
-        for i in range(r):
-            for j in range(c):
-                if visited[i][j] == False and grid[i][j] == 0:
-                    return bfs(i,j)
-        
+        # bfs 시작
+        # 사전 세팅
+        # 이때, 0의 값이면 현재의 길이인 1도 추가로 세팅
+        visited[0][0] = True
+        q = deque()
+        q.append((0, 0, 1))
+        while q:
+            cur_r, cur_c, cur_len = q.popleft() 
+            # 8가지 방향 세팅(상, 상우, 우, 우하, 하, 하좌, 좌, 좌상)
+            dr = [-1, -1, 0, 1, 1, 1, 0, -1]
+            dc = [0, 1, 1, 1, 0, -1, -1, -1]
+            # 현재의 좌표가 (m-1,n-1)이면 반복문 종료
+            if (cur_r, cur_c) == (m-1, n-1):
+                return cur_len
+            # transformed from for v in graph[start_v]
+            for k in range(8):
+                next_r = cur_r + dr[k]
+                next_c = cur_c + dc[k]
+                # 범위 설정
+                # transformed from if v not in visited
+                if 0 <= next_r < m and 0 <= next_c < n:
+                    if grid[next_r][next_c] == 0 and not visited[next_r][next_c]:
+                        visited[next_r][next_c] = True
+                        q.append((next_r, next_c, cur_len + 1))
+        # 인접 노드를 다 순회했는데 종점에 도달하지 못했으면 -1을 리턴
+        return shortest_path
+        # (오답)how 종점에서 반복문 종료? -> 애초에 이중 for문을 사용하는 것이 아니였음!
+        # 이전의 num of islands 문제는 섬들을 찾아야 하기 때문에 완전탐색을 하는 것이 맞지만, 이번 문제는 시점과 종점이 분명하기 때문에 굳이 완전탐색을 할 필요 x => 아직 이중 for문의 사용 유무가 확실하지 않기 때문에 문제를 많이 풀어봐야함!
