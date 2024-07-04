76be05255ff8a48da7287e4360d521d26cd51c56
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-07-04 19:37:26 +0900

    Time: 15 ms (46.89%), Space: 11.6 MB (59.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,21 +1,12 @@
-#
-# @lc app=leetcode id=70 lang=python
-#
-# [70] Climbing Stairs
-#
-
-# @lc code=start
 class Solution(object):
     def climbStairs(self, n):
         memo = {}
-        
-        def dp(n): # 부모의 memo를 사용하기 위해 자식 함수 생성
-            if n == 1 or n == 2:
+
+        def dp(n):
+            if n == 1 or n == 2: # you can either climb 1 or 2 steps
                 return n
             if n not in memo:
-                memo[n] = dp(n-1)+dp(n-2)
+                memo[n] = dp(n-1) + dp(n-2)
             return memo[n]
-        return dp(n) # dp(n)을 호출하면서 n 또는 memo[n]의 결과를 뱉어냄
-    
-sol = Solution()
-print(sol.climbStairs(5))
+        return dp(n)
+        
