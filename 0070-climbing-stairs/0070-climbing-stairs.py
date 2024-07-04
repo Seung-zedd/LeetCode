5ca48314f9ad0e3dbf2c081d68e126c62ecd5906
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-07-04 19:40:33 +0900

    Time: 12 ms (66.3%), Space: 11.5 MB (88.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def climbStairs(self, n):
-        memo = {}
+        table = {1: 1, 2: 2}
 
-        def dp(n):
-            if n == 1 or n == 2: # you can either climb 1 or 2 steps
-                return n
-            if n not in memo:
-                memo[n] = dp(n-1) + dp(n-2)
-            return memo[n]
-        return dp(n)
-        
+        for i in range(3, n+1):
+            table[i] = table[i-1] + table[i-2]
+        return table[n]
