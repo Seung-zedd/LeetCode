ab6d589e68e27f446ecbfc75255281d08931bdf1
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-08-07 19:57:45 +0900

    Time: 498 ms (58.04%), Space: 12.1 MB (62.5%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,39 +1,45 @@
 class Solution(object):
     def shortestPathBinaryMatrix(self, grid):
-        m = len(grid) # row
-        n = len(grid[0]) # col
-        visited = [[False]*n for _ in range(m)]
+        r = len(grid)
+        c = len(grid[0])
         shortest_path = -1
+        visited = [[False]*c for x in range(r)] # 1차원 배열을 col만큼 복사한 후, row만큼 iterate
 
-        # 시점 또는 종점이 1이면 -1을 리턴
-        if grid[0][0] == 1 or grid[m-1][n-1] == 1:
+        # hidden condition_1: 시점이나 종점이 막혀있으면 bfs 호출 불가
+        if grid[0][0] == 1 or grid[r-1][c-1] == 1:
             return shortest_path
 
-        # bfs 시작
-        # 사전 세팅
-        # 이때, 0의 값이면 현재의 길이인 1도 추가로 세팅
-        visited[0][0] = True
-        q = deque()
-        q.append((0, 0, 1))
-        while q:
-            cur_r, cur_c, cur_len = q.popleft() 
-            # 8가지 방향 세팅(상, 상우, 우, 우하, 하, 하좌, 좌, 좌상)
-            dr = [-1, -1, 0, 1, 1, 1, 0, -1]
-            dc = [0, 1, 1, 1, 0, -1, -1, -1]
-            # 현재의 좌표가 (m-1,n-1)이면 반복문 종료
-            if (cur_r, cur_c) == (m-1, n-1):
-                return cur_len
-            # transformed from for v in graph[start_v]
-            for k in range(8):
-                next_r = cur_r + dr[k]
-                next_c = cur_c + dc[k]
-                # 범위 설정
-                # transformed from if v not in visited
-                if 0 <= next_r < m and 0 <= next_c < n:
-                    if grid[next_r][next_c] == 0 and not visited[next_r][next_c]:
-                        visited[next_r][next_c] = True
-                        q.append((next_r, next_c, cur_len + 1))
-        # 인접 노드를 다 순회했는데 종점에 도달하지 못했으면 -1을 리턴
+        # 편의상, a_ij로 표현
+        def bfs(start_i, start_j):
+            # 사전 세팅
+            q = deque()
+            q.append(((start_i, start_j, 1)))
+            visited[start_i][start_j] = True
+
+            while q:
+                cur_i, cur_j, cur_len = q.popleft()
+
+                # 현재 좌표가 종점이면 while문을 종료
+                if (cur_i, cur_j) == (r-1, c-1):
+                    return cur_len
+
+                # 좌상부터 시계방향으로 시작
+                dr = [-1, -1, -1, 0, 1, 1, 1, 0]
+                dc = [-1, 0, 1, 1, 1, 0, -1, -1]
+
+                for x in range(8):
+                    next_i = cur_i + dr[x]
+                    next_j = cur_j + dc[x]
+
+                    # 경계값 범위
+                    if 0 <= next_i < r and 0 <= next_j < c:
+                        if grid[next_i][next_j] == 0 and not visited[next_i][next_j]:
+                            visited[next_i][next_j] = True
+                            q.append((next_i, next_j, cur_len + 1)) # 방문을 했으니까 길이 1 추가
+
+            # bfs를 다 순회했는데 종점에 도달하지 못했으면 -1을 리턴
+            return shortest_path
+            
+        # 드모르간 법칙에 의해 바로 bfs 호출
+        shortest_path = bfs(0,0) # cur_len으로 업데이트
         return shortest_path
-        # (오답)how 종점에서 반복문 종료? -> 애초에 이중 for문을 사용하는 것이 아니였음!
-        # 이전의 num of islands 문제는 섬들을 찾아야 하기 때문에 완전탐색을 하는 것이 맞지만, 이번 문제는 시점과 종점이 분명하기 때문에 굳이 완전탐색을 할 필요 x => 아직 이중 for문의 사용 유무가 확실하지 않기 때문에 문제를 많이 풀어봐야함!
