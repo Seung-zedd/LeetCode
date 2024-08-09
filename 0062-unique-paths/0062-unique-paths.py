977afc0192f1dfeb80cfe45da01fe1130c76ac8e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-08-09 12:43:00 +0900

    Time: 11 ms (82.48%), Space: 11.6 MB (36.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,15 +2,15 @@ class Solution(object):
     def uniquePaths(self, m, n):
         memo = {}
 
-        def dp(x):
+        def dp(i, j):
             # base condition
-            if x <= 1:
+            if i <= 1 or j <= 1:
                 return 1
             # x가 memo에 없으면 fact를 호출
-            if x not in memo:
-                memo[x] = fact(n + m - 2) / (fact(n - 1) * fact(m - 1))
+            if (i,j) not in memo:
+                memo[(i,j)] = fact(n + m - 2) / (fact(n - 1) * fact(m - 1))
             # 그렇지 않으면 memo의 value를 리턴
-            return memo[x]
+            return memo[(i,j)]
 
         def fact(x):
             # base condition: 0!=1, 1!=1
@@ -18,4 +18,4 @@ class Solution(object):
                 return 1
             return fact(x - 1) * x
         
-        return dp(n)
+        return dp(m,n)
