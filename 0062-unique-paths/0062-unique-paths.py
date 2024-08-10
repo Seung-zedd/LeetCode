a2961e1808df4e8e48109c276dbecefc5ae54855
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-08-10 23:03:51 +0900

    Time: 5 ms (97.95%), Space: 11.7 MB (36.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,15 @@
 class Solution(object):
     def uniquePaths(self, m, n):
-        memo = [[-1] * n for _ in range(m)] # col 1차원 배열을 먼저 만들고, row로 iterate
+        table = [[-1] * n for _ in range(m)]
+        
+        # 가장자리 1로 채우기
+        for r in range(m):
+            table[r][0] = 1
+        for c in range(n):
+            table[0][c] = 1
 
-        def dfs(r, c):
-            if (r, c) == (0, 0): # base condition
-                return 1
-            if memo[r][c] == -1:
-            	unique_paths = 0
-    # 경계값을 따로 설정해줘야함(안그러면, 가장자리에 있을때 항상 거짓이되서 NoneType을 반환)
-            	if r - 1 >= 0: # 위쪽 셀에서 오는 경우
-                	unique_paths += dfs(r - 1, c)
-                if c - 1 >= 0: # 왼쪽 셀에서 오는 경우
-                	unique_paths += dfs(r, c - 1)
-                memo[r][c] = unique_paths
-            return memo[r][c]
-        return dfs(m - 1, n - 1)
+        # 이중 for문으로 값 채우기
+        for r in range(1, m):
+            for c in range(1, n):
+                table[r][c] = table[r-1][c] + table[r][c-1]
+        return table[r][c]
