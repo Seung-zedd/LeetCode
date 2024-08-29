253f68f9b46faa734bde8ac8e2371c2953d8001f
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-08-29 16:47:41 +0900

    Time: 14 ms (60.44%), Space: 11.8 MB (16.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def uniquePaths(self, m, n):
-        arr = {}
+        # Bottom-up approach(tabulation)
+        table = {0: 1, 1: 1}
+
         def fact(x):  # 0! or 1! = 1
-            if x <= 1:
-                return 1
-            if x not in arr:  # x가 arr에 없으면 fact를 호출
-                arr[x] = fact(x - 1) * x
-            return arr[x]
+            for i in range(2, x + 1):  # x가 arr에 없으면 fact를 호출
+                table[i] = table[i - 1] * i
+            return table[x]
         # 함수를 먼저 정의해야 함수를 호출할 수 있음
         return fact(m + n - 2) / (fact(m - 1) * fact(n - 1))
