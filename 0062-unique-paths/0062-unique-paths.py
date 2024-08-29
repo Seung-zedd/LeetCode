8b3ba1df22b490274098a258bedeaa6b6ef69ad6
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-08-29 22:49:21 +0900

    Time: 15 ms (52.75%), Space: 12.1 MB (5.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,15 @@
 class Solution(object):
-    def uniquePaths(self, m, n):
-        # Bottom-up approach(tabulation)
-        table = {0: 1, 1: 1}
+    def uniquePaths(self, m, n):  # (m,n)은 row x col
+        memo = {}
 
-        def fact(x):  # 0! or 1! = 1
-            for i in range(2, x + 1):  # x가 arr에 없으면 fact를 호출
-                table[i] = table[i - 1] * i
-            return table[x]
-        # 함수를 먼저 정의해야 함수를 호출할 수 있음
-        return fact(m + n - 2) / (fact(m - 1) * fact(n - 1))
+        def dfs(x, y):  # (x,y)는 좌표임
+            # 경계값 설정(안그러면 stack_over_flow)
+            if x < 0 or y < 0:
+                return 0 # 화살표가 없으니까 0 처리
+            if (x, y) == (0, 0):  # 시작점이면
+                return 1  # 1가지를 리턴(그래야 더해지니까)
+            if (x, y) not in memo:
+                memo[(x, y)] = dfs(x - 1, y) + dfs(x, y - 1)
+            return memo[(x, y)]
+
+        return dfs(m - 1, n - 1)  # 종점의 좌표부터 시작
