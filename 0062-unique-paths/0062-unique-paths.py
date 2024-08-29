0aa6b8d85b5139c3b9eda6c4967e922a1319f2e8
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-08-29 16:38:09 +0900

    Time: 13 ms (65.68%), Space: 11.7 MB (16.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def uniquePaths(self, m, n):
-        table = [[-1] * n for _ in range(m)]
-        
-        # 가장자리 1로 채우기
-        for r in range(m):
-            table[r][0] = 1
-        for c in range(n):
-            table[0][c] = 1
-
-        # 이중 for문으로 값 채우기
-        for r in range(1, m):
-            for c in range(1, n):
-                table[r][c] = table[r-1][c] + table[r][c-1]
-        return table[r][c]
+        arr = {}
+        def fact(x):  # 0! or 1! = 1
+            if x <= 1:
+                return 1
+            if x not in arr:  # x가 arr에 없으면 fact를 호출
+                arr[x] = fact(x - 1) * x
+            return arr[x]
+        # 함수를 먼저 정의해야 함수를 호출할 수 있음
+        return fact(m + n - 2) / (fact(m - 1) * fact(n - 1))
