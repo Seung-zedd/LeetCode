7e8a43e672a2916df5e0982462c07c4f79f3b417
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-09-11 17:44:00 +0900

    Time: 486 ms (12.69%), Space: 13.9 MB (65.08%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -2,19 +2,17 @@ class Solution(object):
     def networkDelayTime(self, times, n, k):
         costs = {}
         pq = []
-        heapq.heappush(pq, (0, k))  # 시작 노드 추가
+        heapq.heappush(pq, (0, k))
 
         while pq:
             cur_cost, cur_v = heapq.heappop(pq)
+
             if cur_v not in costs:
                 costs[cur_v] = cur_cost
-                if len(costs) == n: # 모든 노드를 방문했으면 최소 비용을 리턴
+                if len(costs) == n:
                     return cur_cost
-                # 인접 노드 탐색(먼저 방문을 해야 탐색 시작)
-                for path in times:  # path is row
-                    if cur_v == path[0]:  # 시작 노드인지 확인
-                        cost = path[2]
-                        next_v = path[1]
+                for v, next_v, cost in times:
+                    if v == cur_v:
                         next_cost = cur_cost + cost
                         heapq.heappush(pq, (next_cost, next_v))
         return -1
