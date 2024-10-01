ee48fe4ef52517713a9eb54c0eefee949cdc0ee5
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-10-01 14:42:04 +0900

    Time: 2146 ms (35.13%), Space: 12.4 MB (73.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,7 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        # 1) Nested-loop approach took O(N^2)
-        ans = {} # in order to access indices
-        start = 0
-        for i in range(start, len(nums)):
-            for j in range(start + 1, len(nums)): # to skip overlapped index i
+        # 1) Using nested loops takes O(N^2)
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)): # Use i index and Add 1 to avoid overlapping with i index
                 if nums[i] + nums[j] == target:
-                    ans[i] = nums[i]
-                    ans[j] = nums[j]
-                    break
-            start += 1 # if you can't find the answer, add 1 to i index
-        return list(ans.keys())
+                    return [i, j] # Utilize feature of return
