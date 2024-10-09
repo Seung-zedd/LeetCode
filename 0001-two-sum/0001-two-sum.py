6f60131d54694af9f5f99d32f2822a4ca86ed24e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-10-09 23:43:37 +0900

    Time: 45 ms (65.26%), Space: 12.4 MB (70.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        # 1) Using nested loops takes O(N^2)
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)): # Use i index and Add 1 to avoid overlapping with i index
-                if nums[i] + nums[j] == target:
-                    return [i, j] # Utilize feature of return
+        dict = {} # {number: index} due to return of index
+        
+        for i, num in enumerate(nums):
+            x = target - num
+
+            if x in dict:
+                return [dict[x], i] # {2: 0, 1}
+            dict[num] = i # {2: 0}
