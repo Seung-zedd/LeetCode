88785e552a088e220815aaabf976d305a58d8b6e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-30 13:46:50 +0900

    Time: 9 ms (50.53%), Space: 44.7 MB (94.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,28 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[2];
-
+        // 2D를 HashMap처럼 구현(Key: arr[0] = num, Value: arr[1] = index)
+        int[][] numIndex = new int[nums.length][2]; // {num: index}가 nums의 길이만큼 있음
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    arr[0] = i;
-                    arr[1] = j;
-                    break;
-                }
+            numIndex[i][0] = nums[i];
+            numIndex[i][1] = i;
+        }
+        // 오름차순 정렬
+        Arrays.sort(numIndex, (o1, o2) -> {
+            return o1[0] - o2[0];
+        }); 
+
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+            if (numIndex[l][0] + numIndex[r][0] == target) {
+                return new int[] {numIndex[l][1], numIndex[r][1]};
+            } else if (numIndex[l][0] + numIndex[r][0] > target) {
+                r -= 1;
+            } else {
+                l += 1;
             }
         }
-        return arr;
-
+        return new int[] { -1, -1 };
     }
 }
