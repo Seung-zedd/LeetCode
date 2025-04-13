9ee8d3626180ef870b6fa3e67cd73c88276b3f70
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-04-13 15:01:51 +0900

    Time: 2 ms (97.44%), Space: 42 MB (20.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        List<Character> stack = new ArrayList<>();
+        Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            stack.add(c); // 닫힌 괄호만 주어진 경우도 있기 때문에 무조건 넣어줘야됨
 
-            // 빈 stack을 pop()하지 않기 위해 if 조건 걸어둠
-            // stack.get(stack.lastIndexOf(c) - 1): stack(top - 1)의 원소를 가져옴
-            if (stack.size() > 1) {
-                if (c == ')' && stack.get(stack.lastIndexOf(c) - 1) == '(') {
-                    stack.removeLast();
-                    stack.removeLast();
-                } else if (c == '}' && stack.get(stack.lastIndexOf(c) - 1) == '{') {
-                    stack.removeLast();
-                    stack.removeLast();
-                } else if (c == ']' && stack.get(stack.lastIndexOf(c) - 1) == '[') {
-                    stack.removeLast();
-                    stack.removeLast();
-                }
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return false; // 열린 괄호가 스택에 없으면 false
+            } else if ((c == ')' && stack.peek() == '(') || (c == '}' && stack.peek() == '{') || (c == ']' && stack.peek() == '[')) {
+                stack.pop();
+            } else {
+                return false;
             }
-
         }
-        return stack.isEmpty() ? true : false;
+        return stack.isEmpty();
     }
 }
