485474126f20ba5b904c433edfca30c00340e7dd
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-04-17 03:51:16 +0900

    Time: 5 ms (54.07%), Space: 45 MB (63.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // 2D를 HashMap처럼 구현(Key: arr[0] = num, Value: arr[1] = index)
-        int[][] numIndex = new int[nums.length][2]; // {num: index}가 nums의 길이만큼 있음
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] answer = new int[2];
+
         for (int i = 0; i < nums.length; i++) {
-            numIndex[i][0] = nums[i];
-            numIndex[i][1] = i;
-        }
-        // 오름차순 정렬
-        Arrays.sort(numIndex, (o1, o2) -> {
-            return o1[0] - o2[0];
-        }); 
-
-        int l = 0;
-        int r = nums.length - 1;
-
-        while (l < r) {
-            if (numIndex[l][0] + numIndex[r][0] == target) {
-                return new int[] {numIndex[l][1], numIndex[r][1]};
-            } else if (numIndex[l][0] + numIndex[r][0] > target) {
-                r -= 1;
-            } else {
-                l += 1;
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                answer[0] = map.get(complement);
+                answer[1] = i;
             }
+            map.put(nums[i], i);
         }
-        return new int[] { -1, -1 };
+        return answer;
     }
 }
