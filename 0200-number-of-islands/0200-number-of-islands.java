54fea9e306e312ec96f81cca3bf8ca5fbb7be951
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-05-09 11:24:18 +0900

    Time: 8 ms (14.73%), Space: 52.5 MB (12.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,33 +3,52 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
         int island = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
-                    island += dfs(grid, visited, i, j, m, n);
+                    // bfs 탐색 시작
+                    // 사전 세팅
+                    visited[i][j] = true;
+                    Queue<Pair> q = new LinkedList<>();
+                    q.offer(new Pair(i, j));
+
+                    while (!q.isEmpty()) {
+                        Pair cur = q.poll();
+                        int startR = cur.r;
+                        int startC = cur.c;
+
+                        for (int k = 0; k < 4; k++) {
+                            int nextR = startR + dr[k];
+                            int nextC = startC + dc[k];
+
+                            if ((0 <= nextR) && (nextR < m) && (0 <= nextC) && (nextC < n) && grid[nextR][nextC] == '1' && !visited[nextR][nextC]) {
+                                visited[nextR][nextC] = true;
+                                q.offer(new Pair(nextR, nextC));
+                            }
+                        }
+                    }
+                    island++;
+
                 }
             }
         }
         return island;
     }
 
-    private int dfs(char[][] grid, boolean[][] visited, int startR, int startC, int m, int n) {
-        int[] dr = {1, -1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        visited[startR][startC] = true;
+    private static class Pair {
+        int r;
+        int c;
 
-        for (int k = 0; k < 4; k++) {
-            int nextR = startR + dr[k];
-            int nextC = startC + dc[k];
-
-            if ((0 <= nextR) && (nextR < m) && (0 <= nextC) && (nextC < n) && grid[nextR][nextC] == '1' && !visited[nextR][nextC]) {
-                dfs(grid, visited, nextR, nextC, m, n);
-            }
+        public Pair(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
-        return 1;
     }
+
 
 
 }
