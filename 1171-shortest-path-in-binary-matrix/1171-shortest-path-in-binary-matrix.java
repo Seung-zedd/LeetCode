ae02b83ff1e004487f1f19252af63376364d2920
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-05-09 14:07:09 +0900

    Time: 16 ms (62%), Space: 45.5 MB (65.94%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -12,47 +12,42 @@ class Solution {
             return -1;
         }
 
-        // 완전 탐색
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0 && !visited[i][j]) {
-                    // bfs 탐색 시작
-                    visited[i][j] = true;
-                    Queue<Point> q = new LinkedList<>();
-                    q.offer(new Point(i, j, 1));
+        // (0,0)이 0이면 bfs 탐색 시작
+        if (grid[0][0] == 0) {
+            // bfs 탐색 시작
+            visited[0][0] = true;
+            Queue<Point> q = new LinkedList<>();
+            q.offer(new Point(0, 0, 1));
 
-                    while (!q.isEmpty()) {
-                        Point cur = q.poll();
-                        int startR = cur.r;
-                        int startC = cur.c;
-                        int curCount = cur.count;
+            while (!q.isEmpty()) {
+                Point cur = q.poll();
+                int startR = cur.r;
+                int startC = cur.c;
+                int curCount = cur.count;
 
-                        // 업데이트
-                        count = Math.max(count, curCount);
+                // 업데이트
+                count = Math.max(count, curCount);
+                
+                // 현재 좌표가 종점이면 count를 리턴
+                if ((startR == m - 1) && (startC == n - 1)) {
+                    return count;
+                }
 
-                        // 현재 좌표가 종점이면 count를 리턴
-                        if ((startR == m - 1) && (startC == n - 1)) {
-                            return count;
-                        }
-
-                        for (int k = 0; k < 8; k++) {
-                            int nextR = startR + dr[k];
-                            int nextC = startC + dc[k];
-
-                            if ((nextR >= 0 && nextR < m) && (nextC >= 0 && nextC < n) && grid[nextR][nextC] == 0 && !visited[nextR][nextC]) {
-                                visited[nextR][nextC] = true;
-                                q.offer(new Point(nextR, nextC, curCount + 1));
-                            }
-                        }
+                for (int k = 0; k < 8; k++) {
+                    int nextR = startR + dr[k];
+                    int nextC = startC + dc[k];
+                    
+                    if ((nextR >= 0 && nextR < m) && (nextC >= 0 && nextC < n) && grid[nextR][nextC] == 0 && !visited[nextR][nextC]) {
+                        visited[nextR][nextC] = true;
+                        q.offer(new Point(nextR, nextC, curCount + 1));
                     }
-                    return -1;
-
                 }
             }
+            return -1;
         }
         return count;
     }
-
+    
     private static class Point {
         int r;
         int c;
