9ad1641fda340db38938b3a3a8a13b3a671202aa
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-05-09 10:58:13 +0900

    Time: 8 ms (14.73%), Space: 52.5 MB (12.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,21 +3,18 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
         int island = 0;
 
-        // dr, dc 세팅
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-
-        // 완전 탐색
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                // 섬이고 현재 방문을 하지 않았으면
                 if (grid[i][j] == '1' && !visited[i][j]) {
                     // bfs 탐색 시작
+                    // 사전 세팅
+                    visited[i][j] = true;
                     Queue<Pair> q = new LinkedList<>();
                     q.offer(new Pair(i, j));
-                    visited[i][j] = true;
 
                     while (!q.isEmpty()) {
                         Pair cur = q.poll();
@@ -28,19 +25,15 @@ class Solution {
                             int nextR = startR + dr[k];
                             int nextC = startC + dc[k];
 
-                            if ((0 <= nextR && nextR < m) && (0 <= nextC && nextC < n)) {
-                                if (!visited[nextR][nextC] && grid[nextR][nextC] == '1') {
-                                    visited[nextR][nextC] = true;
-                                    q.offer(new Pair(nextR, nextC));
-                                }
-
+                            if ((0 <= nextR) && (nextR < m) && (0 <= nextC) && (nextC < n) && grid[nextR][nextC] == '1' && !visited[nextR][nextC]) {
+                                visited[nextR][nextC] = true;
+                                q.offer(new Pair(nextR, nextC));
                             }
                         }
                     }
                     island++;
 
                 }
-
             }
         }
         return island;
@@ -55,6 +48,7 @@ class Solution {
             this.c = c;
         }
     }
+
 
 
 }
