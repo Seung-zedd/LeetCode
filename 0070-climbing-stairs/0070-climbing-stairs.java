86b6976422ad9254903437339b15daa9667e29d8
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-05-16 14:35:43 +0900

    Time: 0 ms (100%), Space: 40.7 MB (17.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        // input: steps, output: distinct ways
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        // base condition
-        map.put(1, 1);
-        map.put(2, 2);   
-
-        for (int i = 3; i < n + 1; i++) {
-           map.put(i, map.get(i - 1) + map.get(i - 2));
-        }
-
-        return map.get(n);
-        
-    }
-
+    if (n <= 2) return n; // 1계단은 1가지, 2계단은 2가지
     
+    int[] dp = new int[n + 1];
+    dp[1] = 1; // 1계단은 1가지 방법
+    dp[2] = 2; // 2계단은 2가지 방법
+    
+    for (int i = 3; i < n + 1; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    
+    return dp[n];
+}
+
 }
