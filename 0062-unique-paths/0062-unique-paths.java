962d173bf20db92703a5a0735cd2a07a8cd980f4
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-05-21 13:43:02 +0900

    Time: 0 ms (100%), Space: 40.8 MB (17.64%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // m x n 크기의 dp를 초기화
+        int[][] dp = new int[m][n];
+
+        // 모서리를 1로 모두 채운다.
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // 모서리의 합으로 종점의 값을 리턴
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
