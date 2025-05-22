fe575568c8a1b07d2a48905172731ad1f7ff6c84
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-05-22 13:23:09 +0900

    Time: 0 ms (100%), Space: 40.7 MB (37.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,37 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        // m x n 크기의 dp를 초기화
-        int[][] dp = new int[m][n];
+        public int uniquePaths(int m, int n) {
 
-        // 모서리를 1로 모두 채운다.(시점과 종점이 같을 때도 1을 리턴)
+        int[][] memo = new int[m][n];
         for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
+            Arrays.fill(memo[i], -1);
         }
 
-        // 모서리의 합으로 종점의 값을 리턴
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
-        return dp[m - 1][n - 1];
+        return dp(m - 1, n - 1, memo); // 종점을 리턴
     }
+
+    private static int dp(int r, int c, int[][] memo) {
+        // base condition
+        // 시작점을 1로 설정
+        if (r == 0 && c == 0) {
+            memo[r][c] = 1;
+            return memo[r][c];
+        }
+        int uniquePaths = 0;
+
+        // 범위 설정 및 현재 memo에 값이 없을 때 함수 호출
+        //! 범위 설정을 AND 조건으로 하면 false일 때 중괄호 부분을 실행하지 않기 때문에 따로따로 설정해줘야함
+        if (memo[r][c] == -1) {
+            if (r > 0) {
+                uniquePaths += dp(r - 1, c, memo);
+            }
+            if (c > 0) {
+                uniquePaths += dp(r, c - 1, memo);
+            }
+            memo[r][c] = uniquePaths;
+        }
+
+        return memo[r][c];
+
+    }
+
 }
