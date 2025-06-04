fb9bf8cf94fa0ec32924cabc2a50eedf8979e5da
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-06-04 14:35:53 +0900

    Time: 1080 ms (5.08%), Space: 46.2 MB (5.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,32 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] answer = new int[2];
+        boolean found = false;
+        int[] result = new int[2];
+        backtrack(nums, result, new ArrayList<>(), 0, target, found);
+        return result;
 
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                answer[0] = map.get(complement);
-                answer[1] = i;
+    }
+
+    private static void backtrack(int[] nums, int[] result, List<Integer> curr, int start, int target, boolean found) {
+        // 조기 종료: 이미 답을 찾은 경우
+        if (found) return;
+
+        // base condition
+        if (curr.size() == 2) {
+            if (nums[curr.get(0)] + nums[curr.get(1)] == target) {
+                result[0] = curr.get(0);
+                result[1] = curr.get(1);
+                found = true;
             }
-            map.put(nums[i], i);
+
+            return;
         }
-        return answer;
+
+        // iteration
+        for (int i = start; i < nums.length; i++) {
+            curr.add(i); // 인덱스를 저장
+            backtrack(nums, result, curr, i + 1, target, found);
+            curr.removeLast();
+        }
     }
 }
